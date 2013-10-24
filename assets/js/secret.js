fb78a41f1bdfc0c4ef22cb9d1726ad30419b8abc
Ewetumo Alexander <trinoxf@gmail.com>
2013-10-24 00:23:16 -0700

    Update secret.js

--- assets/js/secret.js
+++ assets/js/secret.js
@@ -6,7 +6,7 @@ localStorage.selectList = JSON.stringify({
 });
 
 localStorage.questionList = JSON.stringify({
-	'qmanager#facebook': "{"0":{"id":0,"question":"what time did you check facebook today?","options":[{"weight":"4","data":"3:00am"}],"answer":null,"checked":false,"color":null}}"
+	
 });
 
 	
